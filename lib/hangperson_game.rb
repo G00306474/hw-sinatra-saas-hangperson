fd918f1864bc2fcb8577aa2b1f8efbef0eb67676
Kevin Moran <g00306474@gmit.ie>
2019-02-10 19:05:27 +0000

    test number fix 3

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -26,6 +26,7 @@ class HangpersonGame
   def guess(a)
     if (a=='' || a== NIL)
         raise ArgumentError, "No null is allowed"
+        return false
     end
     if (a.is_a? Integer)
         raise ArgumentError, "No numbers"
